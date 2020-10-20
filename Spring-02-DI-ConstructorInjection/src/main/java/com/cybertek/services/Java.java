b6f31188ce83dc74bf1a8a70d6264bf192cb0a90
muratkilinc <tarum44@gmail.com>
2020-10-20 18:48:11 -0500

    Dependecy injection

--- Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/services/Java.java
+++ Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/services/Java.java
@@ -3,8 +3,14 @@ package com.cybertek.services;
 import com.cybertek.interfaces.Course;
 
 public class Java implements Course {
+    OfficeHours officeHours;
+
+    public Java(OfficeHours officeHours) {
+        this.officeHours = officeHours;
+    }
+
     @Override
     public void getTeachingHours() {
-        System.out.println("Weekly Teaching Hours:20");
+        System.out.println("Weekly Teaching Hours: "+(20+officeHours.getHours()));
     }
 }
